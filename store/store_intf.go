796e75a5b5d00a5ee1d9d4141f52344ba8cc12df
Hemant Sharma <hemantsharma@Hemants-Laptop.local>
2024-05-21 02:33:24 +0530

    Added protos

--- store/store_intf.go
+++ store/store_intf.go
@@ -1,8 +1,21 @@
 package store
 
+
+
+
 type ProjectStore interface{
+  //@TODO create project struct
+  CreateProject(P) (string, error)
+  GetProject(ProjectId string) (P, error)
+  ListProjects() ([]P, error)
+
 }
+
 type UnitStore interface{
+  //@TODO create unit struct
+  GetUnitsForProject(string) ([]U, error) // preview : get this and return first 5 elements
+  AddUnitToProject(U) (error) // maybe return something as ACK
+
 }
 
 // Store is the main 
