9203bdf318c8674b99900e70675d48132e88e66e
Hemant Sharma <hemantsharma@Hemants-Laptop.local>
2024-05-21 01:26:02 +0530

    Updated store config | WIP

--- store/store_api.go
+++ store/store_api.go
@@ -3,29 +3,51 @@ package store
 import (
 	"database/sql"
 	"sync/atomic"
+  "sync"
 )
 
 type DbConnectionGetter interface {
 	GetDb() (*sql.DB, error)
 }
 
+var dbSingletonInstance *SqliteConnectionImpl
+
 func NewDbConnectionGetter() DbConnectionGetter {
-	return &SqliteConnectionImpl{}
+  if dbSingletonInstance==nil{
+    dbSingletonInstance=&SqliteConnectionImpl{}
+    dbSingletonInstance.db,_=sql.Open("sqlite3", "./unitTracker.db")
+    go dbSingletonInstance.autoCloseDb()
+  }
+	return dbSingletonInstance 
 }
 
 type SqliteConnectionImpl struct {
 	db *sql.DB
 
 	/*
-		when this is 0, we know its safe to close
-      VS. depending on user of this package to call something like db.Close()
+				when this is 0, we know its safe to close
+		      VS. depending on user of this package to call something like db.Close()
 	*/
 
-	referencesHeld int32
+	connectionCount int32
+  wg sync.WaitGroup
+}
+
+func (c *SqliteConnectionImpl ) autoCloseDb(){
+  c.wg.Wait()
+  if atomic.LoadInt32(&c.connectionCount)==0{
+    c.db.Close()
+  }
+}
+
+func (c *SqliteConnectionImpl) CloseDb(){
+  c.wg.Done()
+  atomic.AddInt32(&c.connectionCount, -1)
+  c.db.Close()
 }
 
 func (c *SqliteConnectionImpl) GetDb() (*sql.DB, error) {
-	// @TODO
-	atomic.AddInt32(&c.referencesHeld, 1) // we just gave someone a reference
-	return nil, nil
+  c.wg.Add(1)
+	atomic.AddInt32(&c.connectionCount, 1) // we just gave someone a reference
+	return c.db, nil
 }
