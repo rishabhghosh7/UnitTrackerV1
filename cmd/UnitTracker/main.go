a921a82bd2372c9169b9ab51dbd84ac54f05aa70
Rishabh Ghosh <rishabhghosh515@gmail.com>
2024-05-20 19:57:50 -0400

    CODE : add server / client example

--- cmd/UnitTracker/main.go
+++ cmd/UnitTracker/main.go
@@ -1,12 +1,65 @@
 package main
 
 import (
+	"context"
 	"fmt"
-   "rg/UnitTracker/pkg/proto/appPb"
+	"log"
+	"net"
+	"time"
+
+	"rg/UnitTracker/pkg/proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
+const port = 50051
+
+type server struct {
+	proto.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *proto.HelloRequest) (*proto.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &proto.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func sampleClientCall() {
+	time.Sleep(100 * time.Millisecond)
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := proto.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &proto.HelloRequest{Name: "Randy"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
+
 func main() {
-   fmt.Println("Hello from UT")
-   rpcVar := appPb.HelloReply{Message: "Randy Orton"}
-   fmt.Println("From rpc defns : ", rpcVar.GetMessage())
+	log.Println("Hello from UT")
+
+   go sampleClientCall()
+
+	// setup server
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	proto.RegisterGreeterServer(s, &server{})
+	log.Printf("Listening at %v", listen.Addr())
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve :%v", err)
+	}
+
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 PROTO_SRC := ./proto
-PROTO_OUT := ./pkg/
+PROTO_OUT := ./pkg/proto
 GO_MAIN := ./cmd/UnitTracker/main.go
 GO_OUT := ./bin
 
@@ -12,14 +12,16 @@ PROTOC_GEN_GO_GRPC := protoc-gen-go-grpc
 # Ensuring tools are installed
 .PHONY: tools
 tools:
-	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+	go get google.golang.org/protobuf/cmd/protoc-gen-go@latest
+	go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+	go get google.golang.org/grpc
 
 # Compiling protobuf files
 .PHONY: proto
 proto: tools
 	mkdir -p $(PROTO_OUT)
-	$(PROTOC) --proto_path=$(PROTO_SRC) --go_out=$(PROTO_OUT) --go-grpc_out=$(PROTO_OUT) --go-grpc_opt=paths=source_relative $(PROTO_SRC)/*.proto
+	$(PROTOC) --proto_path=$(PROTO_SRC) --go_out=$(PROTO_OUT) --go-grpc_out=$(PROTO_OUT) \
+		--go_opt=paths=source_relative --go-grpc_opt=paths=source_relative $(PROTO_SRC)/*.proto
 
 # Building the application
 .PHONY: build
@@ -36,6 +38,6 @@ run: build
 # Cleaning up generated files and build artifacts
 .PHONY: clean
 clean:
-	rm -rf $(PROTO_OUT)/*
-	rm -rf $(GO_OUT)
+	rm -rvf $(PROTO_OUT)
+	rm -rvf $(GO_OUT)
 
