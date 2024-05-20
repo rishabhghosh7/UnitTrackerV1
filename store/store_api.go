8a30ecac1308099cff4f3dfe7aed275a2e4448cd
Rishabh Ghosh <rishabhghosh515@gmail.com>
2024-05-20 14:22:19 -0400

    CODE : db singleton initial

--- store/store_api.go
+++ store/store_api.go
@@ -1,5 +1,31 @@
 package store
 
-type DbConnectionGetter {
-  GetDb()
-} 
+import (
+	"database/sql"
+	"sync/atomic"
+)
+
+type DbConnectionGetter interface {
+	GetDb() (*sql.DB, error)
+}
+
+func NewDbConnectionGetter() DbConnectionGetter {
+	return &SqliteConnectionImpl{}
+}
+
+type SqliteConnectionImpl struct {
+	db *sql.DB
+
+	/*
+		when this is 0, we know its safe to close
+      VS. depending on user of this package to call something like db.Close()
+	*/
+
+	referencesHeld int32
+}
+
+func (c *SqliteConnectionImpl) GetDb() (*sql.DB, error) {
+	// @TODO
+	atomic.AddInt32(&c.referencesHeld, 1) // we just gave someone a reference
+	return nil, nil
+}
