9a0f2d46bfb5089c0c727806fe153d2e26890be7
Hemant Sharma <hemantsharma@Hemants-Laptop.local>
2024-05-25 01:49:05 +0530

    Migrations changes

--- store/migrations/02_migration_hs.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-
-INSERT INTO Project (name, desc) VALUES ("sports", "i wanna play football");
-INSERT INTO Project (name, desc) VALUES ("gym", "i wanna lift weights");
-
--- +goose Down
-DELETE * FROM Project WHERE name IN ("sports", "gym");
--- store/migrations/03_migration_hs.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-CREATE TABLE IF NOT EXISTS Unit (
-   project_id INTEGER NOT NULL REFERENCES Project(id),
-   create_ts INTEGER NOT NULL
-);
